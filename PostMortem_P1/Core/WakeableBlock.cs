d7245fe072d8f73875fd7e2bc139623fc8c93b6b
andreyvarlamov-testtesttest <35208582+andreyvarlamov@users.noreply.github.com>
2021-08-15 21:01:41 -0700

    IWakeable and WakeableBlock

---
+++ PostMortem_P1/Core/WakeableBlock.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using PostMortem_P1.Interfaces;
+
+namespace PostMortem_P1.Core
+{
+    public class WakeableBlock : Block, IWakeable
+    {
+        #region IWakeable
+        public long Time { get; }
+
+        public bool WillWakeUp
+        {
+            get
+            {
+                // Override and return true, for children that implement WakeUp
+                return false;
+            }
+            private set
+            {
+            }
+        }
+
+        public long TicksUnloadedAt { get; set; }
+        public virtual void WakeUp(long ticksReloadedAt)
+        {
+
+        }
+        #endregion
+
+        #region WakeableBlock
+        public WakeableBlock(int blockID, string name, Texture2D sprite, bool isAir,
+            bool isWalkable, bool isTransparent, int? itemVersionID) :
+            base(blockID, name, sprite, isAir, isWalkable, isTransparent, itemVersionID)
+        {
+
+        }
+        #endregion
+    }
+}
---
+++ PostMortem_P1/Interfaces/IWakeable.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PostMortem_P1.Interfaces
+{
+    public interface IWakeable : IScheduleable
+    {
+        bool WillWakeUp { get; }
+        long TicksUnloadedAt { get; set; }
+        void WakeUp(long ticksReloadedAt);
+    }
+}
